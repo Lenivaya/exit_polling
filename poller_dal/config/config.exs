a8f3b907fb989066a2e205bb32d40eab58ef053f
Lenivaya <xocada@gmail.com>
2021-03-25 13:43:43 +0200

    Write DAL template

---
+++ poller_dal/config/config.exs
@@ -0,0 +1,10 @@
+import Config
+
+config :poller_dal, PollerDal.Repo,
+  database: "poller_#{Mix.env()}",
+  username: "user",
+  password: "pass",
+  hostname: "localhost"
+
+config :poller_dal,
+  ecto_repos: [PollerDal.Repo]
