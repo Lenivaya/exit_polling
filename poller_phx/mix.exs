c437970205ecc0b173b585b9da2340a02478e91a
Lenivaya <xocada@gmail.com>
2021-03-26 14:48:53 +0200

    Add interaction with PollerDal

--- poller_phx/mix.exs
+++ poller_phx/mix.exs
@@ -41,7 +41,8 @@ defmodule PollerPhx.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:poller_dal, [path: "../poller_dal"]}
     ]
   end
 
