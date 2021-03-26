b56691ed4e27064646f3cae681cb70fd88fa3a81
Lenivaya <xocada@gmail.com>
2021-03-26 17:45:50 +0200

    Add district create form

--- poller_phx/mix.exs
+++ poller_phx/mix.exs
@@ -42,6 +42,7 @@ defmodule PollerPhx.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
+      {:phoenix_ecto, "~> 4.2"},
       {:poller_dal, [path: "../poller_dal"]}
     ]
   end
